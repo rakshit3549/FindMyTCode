d4f71118aea359951ef9d04b519129aa96e9fad3
Rakshit Shetty <32040292+rakshit3549@users.noreply.github.com>
2023-10-09 17:20:48 +0530

    Update jscode.js
    Optimized Search

--- jscode.js
+++ jscode.js
@@ -4,7 +4,7 @@ fetch(`https://docs.google.com/spreadsheets/d/${spreadsheetId}/gviz/tq?tqx=out:j
     .then(res => res.text())
     .then(text => {
         const json = JSON.parse(text.substring(47).slice(0, -2))
-        const jsondata = json['table']["rows"];  
+        const jsondata = json['table']["rows"];
 
         let trHTML = '';
         for (var i = 1; i < jsondata.length; ++i) {
@@ -13,43 +13,43 @@ fetch(`https://docs.google.com/spreadsheets/d/${spreadsheetId}/gviz/tq?tqx=out:j
                 '</td><td class="col3">' + jsondata[i]['c'][1]['v'] +
                 '</td></tr>';
         }
-        $('#tableContent').html(trHTML);
+        var tableContent = document.getElementById('tableContent');
+        tableContent.innerHTML = trHTML;
         trHTML = '';
 
     }).then(() => {
         document.getElementById("table-container").style.visibility = "visible";
     }).then(() => {
 
-        var $rows = $('.table .content-table tr');
+        const rows = document.getElementById('tableContent')
+            .getElementsByTagName('tr');
 
-        $('#search').keyup(function () {
+        let searchTimer;
+        function filterTable() {
 
-            let foundMatch = false;
+            clearTimeout(searchTimer);
+            searchTimer = setTimeout(() => {
+                let foundMatch = false;
+                const searchQuery = document.getElementById('search').value.toLowerCase();
 
-            const searchQuery = $(this).val().toLowerCase();
+                for (let i = 0; i < rows.length; i++) {
+                    const row = rows[i];
+                    const tcode = row.getElementsByTagName('td')[1].innerText.toLowerCase();
+                    const description = row.getElementsByTagName('td')[2].innerText.toLowerCase();
 
-            for (let i = 0; i < $rows.length; i++) {
-                const row = $rows[i];
-                const tcode = row.getElementsByTagName('td')[1].innerText.toLowerCase();
-                const description = row.getElementsByTagName('td')[2].innerText.toLowerCase();
-
-                // Show the row if the search query matches the TCode or Description
-                if (tcode.includes(searchQuery) || description.includes(searchQuery)) {
-                    row.style.display = '';
-                    foundMatch = true;
-                } else {
-                    row.style.display = 'none';
+                    if (tcode.includes(searchQuery) || description.includes(searchQuery)) {
+                        row.style.display = '';
+                        foundMatch = true;
+                    } else {
+                        row.style.display = 'none';
+                    }
                 }
-
                 const table = document.getElementById('table');
                 table.style.display = foundMatch ? '' : 'none';
-            }
+            }, 250);
+        };
 
-
-        });
+        const searchInput = document.getElementById('search');
+        searchInput.addEventListener('input', filterTable);
 
     });
-
-
-
-
